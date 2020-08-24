ae5ff2bee9362c047230901461042774b74d1eb7
makewheels <makewheels>
2020-08-24 21:08:05 +0800

    init

---
+++ src/test/java/com/example/gpstrackerserver/GpsTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gpstrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsTrackerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
